883434ffb75c095e8f5e57544ca6121826751e9e
Paulina Kimak <paulina.kimak.oferty@gmail.com>
2023-09-08 08:36:45 +0200

    'RockPiryt' - create db models, get data from db,
    - create 3 db models: Post, Author, Tag,
    
    - change views to   get data from db, instead of get data from list,

--- my_page/blog/urls.py
+++ my_page/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="all-posts-page"),
-    path("posts/<slug:slug>", views.single_post, name="single-post-page")
+    path("posts/<slug:aslug>", views.single_post, name="single-post-page")
 ]
 
 
--- my_page/blog/views.py
+++ my_page/blog/views.py
@@ -1,92 +1,66 @@
-from datetime import date
-from django.shortcuts import render
+# from datetime import date
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-my_posts = [
-    {
-        "slug": "sleeping-cat",
-        "image": "cat3.jpg",
-        "author": "Paulina",
-        "date": date(2023, 9, 1),
-        "title": "Cats behaviors",
-        "excerpt":"Cats love to eat raw meat.",
-        "content": """Cats love sleep, neque dolor morbi, id semper parturient mauris rutrum integer, urna quam. Purus in praesent nec scelerisque odio, a eu nisl, fermentum odio et amet, bibendum dolor aliquam molestie vitae. Malesuada at nisl dui nulla. Sed morbi, sapien fringilla, accumsan velit dolor neque, vel ut ut voluptatibus, pellentesque dui. Suscipit integer, mi sodales in, non ac augue ridiculus velit ac malesuada, sit quam facilisi odio ac et. 
 
-        Lorem ipsum dolor sit amet, neque dolor morbi, id semper parturient mauris rutrum integer, urna quam. Purus in praesent nec scelerisque odio, a eu nisl, fermentum odio et amet, bibendum dolor aliquam molestie vitae. Malesuada at nisl dui nulla. Sed morbi, sapien fringilla, accumsan velit dolor neque, vel ut ut voluptatibus, pellentesque dui. Suscipit integer, mi sodales in, non ac augue ridiculus velit ac malesuada, sit quam facilisi odio ac et. 
-        
-        Lorem ipsum dolor sit amet, neque dolor morbi, id semper parturient mauris rutrum integer, urna quam. Purus in praesent nec scelerisque odio, a eu nisl, fermentum odio et amet, bibendum dolor aliquam molestie vitae. Malesuada at nisl dui nulla. Sed morbi, sapien fringilla, accumsan velit dolor neque, vel ut ut voluptatibus, pellentesque dui. Suscipit integer, mi sodales in, non ac augue ridiculus velit ac malesuada, sit quam facilisi odio ac et. """
-
-    },
-    {
-        "slug": "cat-are-funny",
-        "image": "cat4.jpg",
-        "author": "Paulina",
-        "date": date(2022, 7, 21),
-        "title": "Funny cats",
-        "excerpt": "Tellus amet sed, malesuada velit ut penatibus eget elit sit, vestibulum est id. .",
-        "content": """
-          Cats are funny adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Paulina",
-        "date": date(2021, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          I love programming consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-]
-
-def get_date(element):
-    # return post.get("date")
-    return element["date"]
 
 # Create your views here.
 
 def starting_page(request):
-    # Sort list my_post, key is function (return value for key date)
-    sorted_posts = sorted(my_posts, key=get_date)
-    # Reverse list
-    latest_post = sorted_posts[-3:]
+    #Last added post on top (only 3 post from database)
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+    
     return render(request, "blog/index.html", {
-        "html_posts": latest_post,
+        "html_posts": latest_posts,
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
-        "html_all_posts": my_posts,
+        "html_all_posts": all_posts,
     })
 
-def single_post(request, slug):
-    # List comprehension and next()
-    # post_detail = next(post for post in my_posts if post["slug"]==slug)
-    #Standard for loop
-    for post in my_posts:
-        if post["slug"] == slug:
-            post_detail = post
+def single_post(request, aslug):
+    #Get single post from db or 404
+    post_detail = get_object_or_404(Post, slug=aslug)
+    # post_detail = Post.objects.get(slug=aslug)
+
+    #Get tags for single post
+    tags_for_post= post_detail.tags.all()
+    
     return render(request, "blog/post-detail.html",{
         "html_post":post_detail,
+        "html_tags_for_post":tags_for_post,
     })
+
+# ######################WITHOUT DATABASE- DATA FROM LIST#############
+# my_posts = []
+
+# def get_date(element):
+#     # return post.get("date")
+#     return element["date"]
+
+# def starting_page(request):
+#     # Sort list my_post, key is function (return value for key date)
+#     sorted_posts = sorted(my_posts, key=get_date)
+#     # Reverse list
+#     latest_post = sorted_posts[-3:]
+#     return render(request, "blog/index.html", {
+#         "html_posts": latest_post,
+#     })
+
+# def posts(request):
+#     return render(request, "blog/all-posts.html", {
+#         "html_all_posts": my_posts,
+#     })
+
+# def single_post(request, slug):
+#     # List comprehension and next()
+#     # post_detail = next(post for post in my_posts if post["slug"]==slug)
+#     #Standard for loop
+#     for post in my_posts:
+#         if post["slug"] == slug:
+#             post_detail = post
+#     return render(request, "blog/post-detail.html",{
+#         "html_post":post_detail,
+#     })
